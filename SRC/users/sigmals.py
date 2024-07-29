63c0c8ff452a060ef74439db6243e65c45981941
Shreyas20004 <shreyas2004src@gmail.com>
2024-07-29 22:11:08 +0530

    second commit

--- SRC/users/sigmals.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import Users
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-from .models import Profile
-
-def create_user_profile( sender, instance,created ,**kwargs):
-    
